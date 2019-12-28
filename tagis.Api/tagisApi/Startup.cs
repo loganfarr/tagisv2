f7fa7e6a94254c6d554db36bd9d9966b8ce30375
Logan Farr <logan@loganfarr.com>
2019-12-28 13:29:28 -0700

    Switched to dotnetcore 2.1 for lambda compatibility

--- tagis.Api/tagisApi/Startup.cs
+++ tagis.Api/tagisApi/Startup.cs
@@ -47,7 +47,7 @@ namespace tagisApi
 
             services.AddDbContext<TagisDbContext>(options => options.UseMySQL(Configuration["ConnectionStrings:Default"]));
             
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
